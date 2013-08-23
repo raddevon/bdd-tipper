520a5b830fea988fd056abd9e66b545f3adfa7be
Devon Campbell <raddevon@gmail.com>
2013-08-23 13:07:14 -0400

    Calculate and display result

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/results', methods=['POST'])
+def results():
+    result = float(request.form['meal_cost']) * \
+        (float(request.form['tip_percentage']) / 100)
+    return render_template('results.html', result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
